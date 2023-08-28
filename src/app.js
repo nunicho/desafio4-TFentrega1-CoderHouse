5f8b9662598e2fccdc46124859bf5d7c54906591
MauricioAlonso <program.alonso@gmail.com>
2023-08-28 17:54:45 -0300

    router productos listo

--- src/app.js
+++ src/app.js
@@ -1,52 +1,20 @@
-// DESAFIO 3  -  ALUMNO:   Mauricio Javier Alonso
-// Comisión: 55565
+const express = require('express')
+const productsRouter = require('./routes/products.router.js');
 
-const express = require("express");
 
-//El servidor importa el archivo Productos.json que es donde se guardan los productos creados por la clase ProductManager.js
-const productos = require("./archivos/productos.json");
-
-const PORT = 3000;
+const PORT=8080;
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-// El servidor cuenta con el endpoint './products'
-app.get("/products", (req, res) => {
-  // Se agregó el soporte para recibir por query param el valor ?limit=
-  const limit = parseInt(req.query.limit) || productos.length;
-  // si se recibe un límite se devuelve el número de productos solicitados.
-  // Y no se recibe un límite se devuelve el listado de todos los productos
-  const limitedData = productos.slice(0, limit);
-  res.json({ status: "ok", data: limitedData });
-});
+app.use('/api/products',productsRouter)
 
-//El servidor cuenta con el endpoint './products/:pid'
+app.use(express.static(__dirname+'/public'))
 
-app.get("/products/:id", (req, res) => {
-  let pid = req.params.id; 
-  pid = parseInt(pid);
-  if (isNaN(pid)) {
-    res.json({
-      status: "error",
-      mensaje: "Require un argumento id de tipo numerico",
-    });
-    return;
-  }
-  let resultado = productos.filter((producto) => producto.id === pid); 
-
-  if (resultado.length > 0) {
-    res.json({ status: "ok", data: resultado });
-  } else {
-    res.json({ status: "error", mensaje: `El id ${pid} no existe` });
-  }
-});
+const server=app.listen(PORT,()=>{
+console.log(`Server escuchando en puerto ${PORT}`);
+})
 
 
-app.get("*", (req, res) => {
-  res.send("Error 404 - page not found");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server corriendo en ${PORT}`);
-});
