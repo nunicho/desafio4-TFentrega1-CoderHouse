e6a5a8f5e9a15b2b5b7694e79ef02942951a7df6
MauricioAlonso <program.alonso@gmail.com>
2023-08-29 01:46:43 -0300

    listas las peticiones del carrito

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const productsRouter = require('./routes/products.router.js');
-
+const cartsRouter = require('./routes/carts.router.js')
 
 const PORT=8080;
 
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use('/api/products',productsRouter)
+app.use("/api/carts", cartsRouter);
 
 app.use(express.static(__dirname+'/public'))
 
---
+++ src/routes/carts.router.js
@@ -0,0 +1,224 @@
+const Router = require("express").Router;
+const router = Router();
+const path = require("path");
+const fs = require("fs");
+
+let ruta = path.join(__dirname, "..", "archivos", "carritos.json");
+let rutaProductos = path.join(__dirname, "..", "archivos", "productos.json");
+
+
+function getCarts() {
+  if (fs.existsSync(ruta)) {
+    return JSON.parse(fs.readFileSync(ruta, "utf-8"));
+  } else {
+    return [];
+  }
+}
+
+
+function getProducts() {
+  if (fs.existsSync(rutaProductos)) {
+    return JSON.parse(fs.readFileSync(rutaProductos, "utf-8"));
+  } else {
+    return [];
+  }
+}
+
+
+
+function saveProducts(carts) {
+  fs.writeFileSync(ruta, JSON.stringify(carts, null, 5));
+}
+
+
+//------------------------------------------------------------------------ PETICION GET
+
+router.get("/", (req, res) => {
+  let carritos = getCarts();
+
+  const limit = parseInt(req.query.limit) || carritos.length;
+  const limitedData = carritos.slice(0, limit);
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json({ data: limitedData });
+});
+
+
+
+
+//------------------------------------------------------------------------ PETICION GET con /:ID
+
+router.get("/:cid/product/:pid", (req, res) => {
+  let carritos = getCarts();
+
+  let cid = req.params.id;
+  cid = parseInt(pid);
+  if (isNaN(pid)) {
+    res.json({
+      status: "error",
+      mensaje: "Require un argumento id de tipo numerico",
+    });
+    return;
+  }
+
+  let resultado = carritos.filter((carrito) => carrito.id === cid);
+
+  if (resultado.length > 0) {
+    res.status(200).json({ data: resultado });
+  } else {
+    res
+      .status(404)
+      .json({ status: "error", mensaje: `El id ${pid} no existe` });
+  }
+});
+
+
+//------------------------------------------------------------------------ PETICION POST
+
+
+router.post("/products/", async (req, res) => {
+  const carritos = getCarts();
+  const productos = getProducts();
+
+  const productsToAdd = req.body;
+
+  if (productsToAdd.length > 0) {
+    let allProductsExist = true;
+
+    for (const product of productsToAdd) {
+      const { id, quantity } = product;
+
+      if (
+        !id ||
+        !quantity ||
+        !productos.some((producto) => producto.id === id)
+      ) {
+        allProductsExist = false;
+        break;
+      }
+    }
+
+    if (allProductsExist) {
+      const groupedProducts = {};
+
+      for (const product of productsToAdd) {
+        const { id, quantity } = product;
+
+        if (!groupedProducts[id]) {
+          groupedProducts[id] = quantity;
+        } else {
+          groupedProducts[id] += quantity;
+        }
+      }
+
+      const updatedCarrito = {
+        id: carritos.length > 0 ? carritos[carritos.length - 1].id + 1 : 1,
+        products: Object.keys(groupedProducts).map((id) => ({
+          id: parseInt(id),
+          quantity: groupedProducts[id],
+        })),
+      };
+
+      carritos.push(updatedCarrito);
+
+      try {
+        await fs.promises.writeFile(
+          ruta,
+          JSON.stringify(carritos, null, 2),
+          "utf8"
+        );
+        res.status(201).json(updatedCarrito); // Agregar respuesta en caso de éxito
+      } catch (error) {
+        res.status(500).json({ error: "Error al guardar el carrito" });
+      }
+    } else {
+      res
+        .status(400)
+        .json({ error: "Algunos productos no existen o datos incompletos" });
+    }
+  } else {
+    res.status(400).json({ error: "Debe enviar al menos un producto" });
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+/*
+
+router.post("/products/", async (req, res) => {
+  const carritos = getCarts();
+  const productos = getProducts();
+
+  const productsToAdd = req.body;
+
+  if (productsToAdd.length > 0) {
+    let allProductsExist = true;
+
+    for (const product of productsToAdd) {
+      const { id, quantity } = product;
+
+      if (
+        !id ||
+        !quantity ||
+        !productos.some((producto) => producto.id === id)
+      ) {
+        allProductsExist = false;
+        break;
+      }
+    }
+
+    if (allProductsExist) {
+      for (const product of productsToAdd) {
+        const { id, quantity } = product;
+        const existingCarritoIndex = carritos.findIndex((carrito) =>
+          carrito.products.some((p) => p.id === id)
+        );
+
+        if (existingCarritoIndex !== -1) {
+          const existingProductIndex = carritos[
+            existingCarritoIndex
+          ].products.findIndex((p) => p.id === id);
+          if (existingProductIndex !== -1) {
+            carritos[existingCarritoIndex].products[
+              existingProductIndex
+            ].quantity += quantity;
+          } else {
+            carritos[existingCarritoIndex].products.push({ id, quantity });
+          }
+        } else {
+          const nuevoCarrito = {
+            id: carritos.length > 0 ? carritos[carritos.length - 1].id + 1 : 1,
+            products: [{ id, quantity }],
+          };
+          carritos.push(nuevoCarrito);
+        }
+      }
+
+      try {
+        await fs.promises.writeFile(
+          ruta,
+          JSON.stringify(carritos, null, 2),
+          "utf8"
+        );
+        res.status(201).json(carritos); // Agregar respuesta en caso de éxito
+      } catch (error) {
+        res.status(500).json({ error: "Error al guardar el carrito" });
+      }
+    } else {
+      res
+        .status(400)
+        .json({ error: "Algunos productos no existen o datos incompletos" });
+    }
+  } else {
+    res.status(400).json({ error: "Debe enviar al menos un producto" });
+  }
+});
+
+
+
+
+
+*/
